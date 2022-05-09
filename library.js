05e788d7dd371245114590116d228e9f7e0e699c
Oliver2-web <105193155+Oliver2-web@users.noreply.github.com>
2022-05-09 19:18:45 +0800

    Update library.js

--- library.js
+++ library.js
@@ -13,9 +13,6 @@ plugin.init = async (params) => {
 
 	/**
 	 * We create two routes for every view. One API call, and the actual route itself.
-	 * Use the `setupPageRoute` helper and NodeBB will take care of everything for you.
-	 *
-	 * Other helpers include `setupAdminPageRoute` and `setupAPIRoute`
 	 * */
 	routeHelpers.setupPageRoute(router, '/quickstart', middleware, [(req, res, next) => {
 		winston.info(`[plugins/quickstart] In middleware. This argument can be either a single middleware or an array of middlewares`);
@@ -28,31 +25,7 @@ plugin.init = async (params) => {
 	routeHelpers.setupAdminPageRoute(router, '/admin/plugins/quickstart', middleware, [], controllers.renderAdminPage);
 };
 
-/**
- * If you wish to add routes to NodeBB's RESTful API, listen to the `static:api.routes` hook.
- * Define your routes similarly to above, and allow core to handle the response via the
- * built-in helpers.formatApiResponse() method.
- *
- * In this example route, the `ensureLoggedIn` middleware is added, which means a valid login
- * session or bearer token (which you can create via ACP > Settings > API Access) needs to be
- * passed in.
- *
- * To call this example route:
- *   curl -X GET \
- * 		http://example.org/api/v3/plugins/quickstart/test \
- * 		-H "Authorization: Bearer some_valid_bearer_token"
- *
- * Will yield the following response JSON:
- * 	{
- *		"status": {
- *			"code": "ok",
- *			"message": "OK"
- *		},
- *		"response": {
- *			"foobar": "test"
- *		}
- *	}
- */
+
 plugin.addRoutes = async ({ router, middleware, helpers }) => {
 	const middlewares = [
 		middleware.ensureLoggedIn,			// use this if you want only registered users to call this route
